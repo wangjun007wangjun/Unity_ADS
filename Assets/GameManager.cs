fd1bf008d0bd761cabbbfe8d42b401c687b153fd
wangjun007wangjun <1334146801@qq.com>
2018-10-27 09:46:50 +0800

    unity ads

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class GameManager : MonoBehaviour {
+
+	void Start()
+	{
+		Advertisement.Initialize ("1723177");
+	}
+	public void OnshowAdsButtonClick()
+	{
+		if (Advertisement.IsReady ()) {
+			Advertisement.Show ();
+		}
+	}
+}
